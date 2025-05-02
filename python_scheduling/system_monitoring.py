4e7253fe57b05b2d38f15bcb231f4f71266f6326
joaoborges99 <borgesvictorj@gmail.com>
2025-05-02 14:43:23 -0300

    foi

--- python_scheduling/system_monitoring.py
+++ python_scheduling/system_monitoring.py
@@ -1,4 +1,4 @@
-from rocketry import rocketry
+from rocketry import Rocketry
 from rocketry.conds import cron
 from datetime import datetime as dt
 
@@ -16,5 +16,28 @@ def transformaData (compInfo:dict):
     logging.warning('Transformando os dados de CPU e RAM')
     update_time = dt.now().strftime('%d/%m/%Y %H:%M:%S')
     compInfo['updated_at'] = update_time
-    return list.append(compInfo)
-    
+    return [compInfo]
+
+
+def saveData (data:dict):
+    file_path = './saved_data/system_monitor.csv'
+    file_exists = os.path.exists(file_path)
+    with open(file_path, 'a', encoding='UTF-8') as file:
+        columns = ['ram', 'cpu', 'updated_at']
+        writer = csv.DictWriter(file, fieldnames=columns)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerows(data)
+
+    return True
+
+
+@app.task(cron('* * * * *'))
+
+def task ():
+    data = getComponentsUsage()
+    system_usage = transformaData(data)
+    saveData(system_usage)
+
+if __name__ == '__main__':
+    app.run()
